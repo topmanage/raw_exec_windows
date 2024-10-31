0be7d87f5cfb20c59c939bc5d70d5385251c11d8
HaimKortovichTM <haim.kortovich@topmanage.com>
2024-10-31 14:48:21 -0500

    use job objects

--- executor/executor_windows.go
+++ executor/executor_windows.go
@@ -16,120 +16,74 @@ import (
 	"unsafe"
 )
 
-// configure new process group for child process
+// configure new process group for child process and creates a JobObject for the
+// executor. Children of the executor will be created in the same JobObject
+// Ref: https://learn.microsoft.com/en-us/windows/win32/procthread/job-objects
 func (e *UniversalExecutor) setNewProcessGroup() error {
 	// We need to check that as build flags includes windows for this file
 	if e.childCmd.SysProcAttr == nil {
 		e.childCmd.SysProcAttr = &syscall.SysProcAttr{}
 	}
 	e.childCmd.SysProcAttr.CreationFlags = syscall.CREATE_NEW_PROCESS_GROUP
-	return nil
-}
 
-// Cleanup any still hanging user processes
-// func (e *UniversalExecutor) killProcessTree(proc *os.Process) error {
-// 	// We must first verify if the process is still running.
-// 	// (Windows process often lingered around after being reported as killed).
-// 	handle, err := syscall.OpenProcess(syscall.PROCESS_TERMINATE|syscall.SYNCHRONIZE|syscall.PROCESS_QUERY_INFORMATION, false, uint32(proc.Pid))
-// 	if err != nil {
-// 		return os.NewSyscallError("OpenProcess", err)
-// 	}
-// 	defer syscall.CloseHandle(handle)
-
-// 	result, err := syscall.WaitForSingleObject(syscall.Handle(handle), 0)
-
-//		switch result {
-//		case syscall.WAIT_OBJECT_0:
-//			return nil
-//		case syscall.WAIT_TIMEOUT:
-//			// Process still running.  Just kill it.
-//			return proc.Kill()
-//		default:
-//			return os.NewSyscallError("WaitForSingleObject", err)
-//		}
-//	}
-//
-
-// Function to recursively get all child process IDs
-func getChildProcessesRecursive(parentPid int) ([]int, error) {
-	var allChildPids []int
-
-	// Create a snapshot of all processes
-	handle, err := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, 0)
+	// note: we don't call CloseHandle on this job handle because we need to
+	// hold onto it until the executor exits
+	job, err := windows.CreateJobObject(nil, nil)
 	if err != nil {
-		return nil, err
-	}
-	defer windows.CloseHandle(handle)
-
-	var processEntry windows.ProcessEntry32
-	processEntry.Size = uint32(unsafe.Sizeof(processEntry))
-
-	// Iterate through the processes
-	if err = windows.Process32First(handle, &processEntry); err != nil {
-		return nil, err
+		return fmt.Errorf("could not create Windows job object for executor: %w", err)
 	}
 
-	for {
-		if int(processEntry.ParentProcessID) == parentPid {
-			childPid := int(processEntry.ProcessID)
-			// Add the child PID to the list
-			allChildPids = append(allChildPids, childPid)
-
-			// Recursively get child processes of this child
-			childDescendants, err := getChildProcessesRecursive(childPid)
-			if err != nil {
-				return nil, err
-			}
-			// Add all descendants to the list
-			allChildPids = append(allChildPids, childDescendants...)
-		}
-		if err = windows.Process32Next(handle, &processEntry); err != nil {
-			if err == windows.ERROR_NO_MORE_FILES {
-				break
-			}
-			return nil, err
-		}
+	info := windows.JOBOBJECT_EXTENDED_LIMIT_INFORMATION{
+		BasicLimitInformation: windows.JOBOBJECT_BASIC_LIMIT_INFORMATION{
+			LimitFlags: windows.JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE,
+		},
 	}
-
-	return allChildPids, nil
-}
-
-// Function to kill a process by its PID
-func killProcess(pid int) error {
-	// Open the process with termination rights
-	handle, err := windows.OpenProcess(windows.PROCESS_TERMINATE, false, uint32(pid))
+	_, err = windows.SetInformationJobObject(
+		job,
+		windows.JobObjectExtendedLimitInformation,
+		uintptr(unsafe.Pointer(&info)),
+		uint32(unsafe.Sizeof(info)))
 	if err != nil {
-		return err
+		return fmt.Errorf("could not configure Windows job object for executor: %w", err)
 	}
-	defer windows.CloseHandle(handle)
 
-	// Terminate the process
-	err = windows.TerminateProcess(handle, 0)
+	handle := windows.CurrentProcess()
+	err = windows.AssignProcessToJobObject(job, handle)
 	if err != nil {
-		return err
+		return fmt.Errorf("could not assign executor to Windows job object: %w", err)
 	}
 
 	return nil
 }
 
+// Cleanup any still hanging user processes
 func (e *UniversalExecutor) killProcessTree(proc *os.Process) error {
-	// Get all child process IDs (including descendants)
-	childPids, err := getChildProcessesRecursive(proc.Pid)
+	// We must first verify if the process is still running.
+	// (Windows process often lingered around after being reported as killed).
+	handle, err := syscall.OpenProcess(syscall.PROCESS_TERMINATE|syscall.SYNCHRONIZE|syscall.PROCESS_QUERY_INFORMATION, false, uint32(proc.Pid))
 	if err != nil {
-		e.logger.Warn("Error gettings child processes: %v", err)
+		return os.NewSyscallError("OpenProcess", err)
 	}
+	defer syscall.CloseHandle(handle)
 
-	// Kill each process and ignore any errors
-	for _, pid := range childPids {
-		err := killProcess(pid)
-		if err != nil {
-			e.logger.Warn("Error killing process: %v", err)
-		}
+	result, err := syscall.WaitForSingleObject(syscall.Handle(handle), 0)
+
+	switch result {
+	case syscall.WAIT_OBJECT_0:
+		return nil
+	case syscall.WAIT_TIMEOUT:
+		// Process still running.  Just kill it.
+		return proc.Kill()
+	default:
+		return os.NewSyscallError("WaitForSingleObject", err)
 	}
+}
 
-	err = killProcess(proc.Pid)
+// Send a Ctrl-Break signal for shutting down the process,
+func sendCtrlBreak(pid int) error {
+	err := windows.GenerateConsoleCtrlEvent(syscall.CTRL_BREAK_EVENT, uint32(pid))
 	if err != nil {
-		e.logger.Warn("Error killing process: %v", err)
+		return fmt.Errorf("Error sending ctrl-break event: %v", err)
 	}
 	return nil
 }
@@ -157,7 +111,6 @@ func (e *UniversalExecutor) shutdownProcess(s os.Signal, proc *os.Process, shutd
 
 	return nil
 }
-
 func (e *UniversalExecutor) sendShutdown(proc *os.Process, url string) error {
 	method := "POST"
 	payload := []byte{}
@@ -174,24 +127,6 @@ func (e *UniversalExecutor) sendShutdown(proc *os.Process, url string) error {
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("Error returned non 200 code: %v", err)
-	}
-	return nil
-}
-
-// Send a Ctrl-C signal for shutting down the process,
-func sendCtrlC(pid int) error {
-	err := windows.GenerateConsoleCtrlEvent(syscall.CTRL_C_EVENT, uint32(pid))
-	if err != nil {
-		return fmt.Errorf("Error sending ctrl-c event: %v", err)
-	}
-	return nil
-}
-
-// Send a Ctrl-Break signal for shutting down the process,
-func sendCtrlBreak(pid int) error {
-	err := windows.GenerateConsoleCtrlEvent(syscall.CTRL_BREAK_EVENT, uint32(pid))
-	if err != nil {
-		return fmt.Errorf("Error sending ctrl-break event: %v", err)
 	}
 	return nil
 }
